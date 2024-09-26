f4cf3b84d009b223fdc90a816cdbf046689efa60
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-26 19:24:28 -0300

    Cria as variaveis

---
+++ index.js
@@ -0,0 +1,7 @@
+const update = () => {
+    const bill = Number(document.getElementById('yourBill').value);
+    const tipPercent = document.getElementById('tipInput').value;
+    const split = document.getElementById('splitInput').value;
+
+    console.log(tipPercent)
+};
