1b167aa232aefa3409142a61637f47d9aa7d3a57
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-26 20:00:34 -0300

    End Project

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const update = () => {
     const tipPercent = document.getElementById('tipInput').value;
     const split = document.getElementById('splitInput').value;
 
-    console.log(tipPercent)
+    const tipValue = bill * (tipPercent / 100);
+    const billTotal = bill + tipValue;
+    const billEach = billTotal / split;
+
+    document.getElementById('tipPercent') .innerHTML = tipPercent + '%';
+    document.getElementById('splitValue') .innerHTML = split + ' Person';
+
+    document.getElementById('tipValue').innerHTML = tipValue.toFixed(2) + '$';
+    document.getElementById('totalWithTip').innerHTML = billTotal.toFixed(2) + '$';
+    document.getElementById('billEach').innerHTML = billEach.toFixed(2) + '$';
+
 };
