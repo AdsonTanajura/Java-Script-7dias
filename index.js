831cdb4ed7e7accb23249675a656e412750ef8a2
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-26 20:23:03 -0300

    End Project

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const update = () => {
-    const bill = Number(document.getElementById('yourBill').value);
-    const tipPercent = document.getElementById('tipInput').value;
-    const split = document.getElementById('splitInput').value;
+const colors = [
+    "red", 
+    "blue", 
+    "green", 
+    "yellow", 
+    "purple", 
+    "orange", 
+    "pink", 
+    "brown", 
+    "black", 
+    "white", 
+    "gray", 
+    "cyan", 
+    "magenta", 
+    "lime", 
+    "olive", 
+    "navy", 
+    "teal", 
+    "maroon", 
+    "violet", 
+    "indigo"
+  ];
+  const body = document.getElementById('body');
 
-    const tipValue = bill * (tipPercent / 100);
-    const billTotal = bill + tipValue;
-    const billEach = billTotal / split;
-
-    document.getElementById('tipPercent') .innerHTML = tipPercent + '%';
-    document.getElementById('splitValue') .innerHTML = split + ' Person';
-
-    document.getElementById('tipValue').innerHTML = tipValue.toFixed(2) + '$';
-    document.getElementById('totalWithTip').innerHTML = billTotal.toFixed(2) + '$';
-    document.getElementById('billEach').innerHTML = billEach.toFixed(2) + '$';
-
-};
+  const changeColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    body.style.backgroundColor = colors[randomIndex];
+  };
